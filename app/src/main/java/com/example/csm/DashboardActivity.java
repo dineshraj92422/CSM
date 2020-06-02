b036e9df378f4b0396182e89cd39c3fd09a101bf
dineshraj92422 <dineshraj9724226506@gmail.com>
2020-06-02 14:34:41 +0530

    First Commit

---
+++ app/src/main/java/com/example/csm/DashboardActivity.java
@@ -0,0 +1,77 @@
+package com.example.csm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.csm.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+        ImageView imgAssin = (ImageView) findViewById(R.id.imgassign);
+        imgAssin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, AssignmentActivity.class );
+                startActivity( intent);
+            }
+        });
+
+        TextView txtAssin = (TextView) findViewById(R.id.txtassing);
+        txtAssin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, AssignmentActivity.class );
+                startActivity( intent);
+            }
+        });
+
+        ImageView imgAtten = (ImageView) findViewById(R.id.imgatten);
+        imgAtten.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, AttendanceActivity.class );
+                startActivity( intent);
+            }
+        });
+
+
+        TextView txtAtten = (TextView) findViewById(R.id.txtatten);
+        txtAtten.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, AttendanceActivity.class );
+                startActivity( intent);
+            }
+        });
+
+        ImageView imgTime = (ImageView) findViewById(R.id.imgtime);
+        imgTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, TimeTableActivity.class );
+                startActivity( intent);
+            }
+        });
+
+
+        TextView txtTime = (TextView) findViewById(R.id.txttime);
+        txtTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, TimeTableActivity.class );
+                startActivity( intent);
+            }
+        });
+    }
+}
