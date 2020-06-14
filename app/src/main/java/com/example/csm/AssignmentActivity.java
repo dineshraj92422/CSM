e45a0d0f845ff75264dc66441ddfd03137e8d21c
dineshraj92422 <dineshraj9724226506@gmail.com>
2020-06-14 09:49:00 +0530

    Assignment Cards Completed

--- app/src/main/java/com/example/csm/AssignmentActivity.java
+++ app/src/main/java/com/example/csm/AssignmentActivity.java
@@ -1,14 +1,89 @@
 package com.example.csm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class AssignmentActivity extends AppCompatActivity {
+    ArrayList<AssingmentsItem> mAssignmentList;
+
+    private RecyclerView mRecyclerView;
+    private AssingmentAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private Button mInsert;
+    private EditText msub;
+    private EditText mdead;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignment);
+
+        createAssignmentList();
+        buildRecyclerView();
+        setButtons();
+
+
+    }
+
+    public void insertItem(String sub,String dead){
+        mAssignmentList.add(new AssingmentsItem(R.drawable.ic_assignment,sub,dead));
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public void removeItem(int Position){
+        mAssignmentList.remove(Position);
+        mAdapter.notifyItemRemoved(Position);
+    }
+
+    public void createAssignmentList(){
+        mAssignmentList = new ArrayList<>();
+
+
+
+    }
+
+    public void buildRecyclerView(){
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new AssingmentAdapter(mAssignmentList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setOnItemClickListner(new AssingmentAdapter.onItemClickListner() {
+            @Override
+            public void onDeleteClick(int position) {
+                 removeItem(position);
+            }
+        });
+    }
+
+    public void setButtons(){
+        msub=findViewById(R.id.edittxt_insert);
+        mdead=findViewById(R.id.edittxt_deadline);
+        mInsert=findViewById(R.id.btn_insert);
+        mInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sub = msub.getText().toString();
+                String dead = mdead.getText().toString();
+                insertItem(sub,dead);
+            }
+        });
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/csm/AssingmentAdapter.java
@@ -0,0 +1,90 @@
+package com.example.csm;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AssingmentAdapter extends RecyclerView.Adapter<AssingmentAdapter.AssingmentViemHolder> {
+
+    private ArrayList<AssingmentsItem> mAssingmentsList;
+    private onItemClickListner mListner;
+
+    public void setOnItemClickListner(onItemClickListner listner){
+        mListner = listner;
+    }
+
+    public interface onItemClickListner{
+        void onDeleteClick(int position);
+    }
+
+    public static class AssingmentViemHolder extends RecyclerView.ViewHolder {
+
+        public ImageView mImageView;
+        public TextView mTextView1;
+        public TextView mTextView2;
+        public ImageView mDelete;
+
+
+        public AssingmentViemHolder(@NonNull View itemView, final onItemClickListner listner) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imgassiv);
+            mTextView1 = itemView.findViewById(R.id.txtv1);
+            mTextView2 = itemView.findViewById(R.id.txtv2);
+            mDelete = itemView.findViewById(R.id.img_delete);
+
+
+           mDelete.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   if (listner != null) {
+                       int position = getAdapterPosition();
+                       if (position != RecyclerView.NO_POSITION) {
+                           listner.onDeleteClick(position);
+                       }
+                   }
+               }
+           });
+
+        }
+    }
+
+    public AssingmentAdapter(ArrayList<AssingmentsItem> assingmentsList) {
+
+        mAssingmentsList = assingmentsList;
+
+    }
+
+    @NonNull
+    @Override
+    public AssingmentViemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.assignments_item, parent, false);
+        AssingmentViemHolder avh = new AssingmentViemHolder(v,mListner);
+        return avh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AssingmentViemHolder holder, int position) {
+
+        AssingmentsItem currentItem = mAssingmentsList.get(position);
+
+        holder.mImageView.setImageResource(currentItem.getImageResource());
+        holder.mTextView1.setText(currentItem.getText1());
+        holder.mTextView2.setText(currentItem.getText2());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mAssingmentsList.size();
+    }
+}
